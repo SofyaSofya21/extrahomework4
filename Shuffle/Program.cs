e76e1f571952807e85157b48cbcce998e9c35332
Sofya <sofyaguseva1@gmail.comgit>
2022-11-27 03:16:48 +0100

    Shuffle method created

--- Shuffle/Program.cs
+++ Shuffle/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+
+Console.WriteLine("Введите желаемую длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int minValue = -99;
+int maxValue = 99;
+int additionalValue = maxValue + 1; // создаю доп число, которого точно не будет в массиве
+
+int[] numbers = new int[size];
+numbers = FillArrayRandom(numbers, minValue, maxValue);
+
+int[] numbersShuffled = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    while(true)
+    {
+        numbersShuffled[i] = new Random().Next(minValue, maxValue+1);
+        for (int j = 0; j < size; j++)
+        {
+            if (numbersShuffled[i] == numbers[j])
+            {
+                numbers[j] = additionalValue;
+                break;
+            }
+        }
+    }
+}
+
+Console.Write("["+String.Join(", ", numbers)+ "] -> "+"["+String.Join(", ", numbersShuffled)+ "]");
+Console.WriteLine();
+
+int[] FillArrayRandom(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
---
+++ Shuffle2/Program.cs
@@ -0,0 +1,70 @@
+﻿// 7. Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+
+Console.WriteLine("Введите желаемую длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int minValue = 0;
+int maxValue = 99;
+
+int[] numbers = new int[size];
+numbers = FillArrayRandom(numbers, minValue, maxValue);
+
+int[] numbersShuffled = new int[size];
+numbersShuffled = ShuffleArray(numbers);
+
+Console.Write("[" + String.Join(", ", numbers) + "] -> " + "[" + String.Join(", ", numbersShuffled) + "]");
+Console.WriteLine();
+
+
+int[] FillArrayRandom(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] RemoveFromArrayNumber(int[] array, int removeNumber)
+{
+    int i = 0;
+    int[] arrayNew = new int[array.Length - 1];
+    int removePosIndex = IndexOf(array, removeNumber);
+    for (i = 0; i < arrayNew.Length; i++)
+    {
+        if (i >= removePosIndex)
+            arrayNew[i] = array[i + 1];
+        else
+            arrayNew[i] = array[i];
+    }
+    return arrayNew;
+}
+
+int IndexOf(int[] array, int find)
+{
+    int i = 0;
+    int position = -1;
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array[i] == find)
+        {
+            position = i;
+            break;
+        }
+    }
+    return position;
+}
+
+int[] ShuffleArray(int[] array)
+{
+    int[] tempArray = array;
+    int[] numbersMixed = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int randomIndex = new Random().Next(0, tempArray.Length);
+        numbersMixed[i] = tempArray[randomIndex];
+        tempArray = RemoveFromArrayNumber(tempArray, numbersMixed[i]);
+    }
+    return numbersMixed;
+}
+
